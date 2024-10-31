9c1bbbf3445a69239a90c8b076119f19f13cb6ec
jindrahabarta <jindriskuv@email.cz>
2024-10-31 19:57:08 +0100

    trying to add calculator

--- src/app/poptat-tisk/page.tsx
+++ src/app/poptat-tisk/page.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 const Calculator = () => {
     return (
         <div className='mt-40'>
-            <div className='bg-transparent dark:bg-transparent'>
-                <iframe
-                    src='https://3dobjectcalculator.netlify.app'
-                    name='iframe test'
-                    className='w-full'
-                    scrolling='no'
-                    height='800px'
-                ></iframe>
+            <div className='flex justify-center'>
+                <div className='shadow-md bg-gray-100/50 dark:bg-gray-200/40 border-gray-400/50 dark:border-gray-800/50 border-2 rounded-2xl p-2 w-[450px] h-fit'>
+                    <iframe
+                        src='https://3dobjectcalculator.netlify.app'
+                        name='Calculator iframe'
+                        className='w-full'
+                        height='700px'
+                    ></iframe>
+                </div>
             </div>
         </div>
     )

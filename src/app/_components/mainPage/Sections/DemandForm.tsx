bb38e98e7d272f2083ba2bbc2bb50b1e4c33fc7f
printujto <printujtocz@gmail.com>
2024-11-08 21:34:45 +0100

    Update DemandForm.tsx

--- src/app/_components/mainPage/Sections/DemandForm.tsx
+++ src/app/_components/mainPage/Sections/DemandForm.tsx
@@ -9,19 +9,21 @@ const DemandForm = () => {
         >
             <h1 className='text-center'>Máte zájem o naše služby?</h1>
 
-            <p className='w-full sm:w-2/3'>
-                Vyplňte prosím následující formulář a my vás co nejdříve kontaktujeme s cenovou nabídkou.
-            </p>
-            <p className='w-full sm:w-2/3'>
-                Do zprávy můžete uvést podrobnosti o vašem projektu. Pokud máte k dispozici fotografie nebo technickou dokumentaci (ve formátech JPG, JPEG, PNG, GIF nebo PDF), můžete je přiložit pomocí funkce nahrávání souborů.
-            </p>
-            <p className='w-full sm:w-2/3'>
-                Pokud již máte svůj 3D model (STL), můžete jej nahrát prostřednictvím našeho{' '}
-                <Link href={'/poptat-tisk'}>
-                    Kalkulátoru
-                </Link>{' '}
-                a získat okamžitou cenovou nabídku.
-            </p>
+            <div className='w-full max-w-xl'>
+                <p>
+                    Vyplňte prosím následující formulář a my vás co nejdříve kontaktujeme s cenovou nabídkou.
+                </p>
+                <p className='w-full sm:w-2/3'>
+                    Do zprávy můžete uvést podrobnosti o vašem projektu. Pokud máte k dispozici fotografie nebo technickou dokumentaci (ve formátech JPG, JPEG, PNG, GIF nebo PDF), můžete je přiložit pomocí funkce nahrávání souborů.
+                </p>
+                <p className='w-full sm:w-2/3'>
+                    Pokud již máte svůj 3D model (STL), můžete jej nahrát prostřednictvím našeho{' '}
+                    <Link href={'/poptat-tisk'}>
+                        Kalkulátoru
+                    </Link>{' '}
+                    a získat okamžitou cenovou nabídku.
+                </p>
+            </div>
 
             <div className='w-full max-w-xl'>
                 <div className=' shadow-md bg-gray-100/50 dark:bg-gray-200/40 border-gray-400/50 dark:border-gray-800/50 border-2 rounded-2xl p-2'>

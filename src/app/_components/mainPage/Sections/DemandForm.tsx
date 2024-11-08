8348866acc488546da0b986aed6a6acb8d051f0c
printujto <printujtocz@gmail.com>
2024-11-08 21:41:08 +0100

    Update DemandForm.tsx

--- src/app/_components/mainPage/Sections/DemandForm.tsx
+++ src/app/_components/mainPage/Sections/DemandForm.tsx
@@ -9,14 +9,14 @@ const DemandForm = () => {
         >
             <h1 className='text-center'>Máte zájem o naše služby?</h1>
 
-            <div className='w-full max-w-xl'>
-                <p>
+            <div className='w-full max-w-xl text-center'>
+                <p className='mt-4'>
                     Vyplňte prosím následující formulář a my vás co nejdříve kontaktujeme s cenovou nabídkou.
                 </p>
-                <p className='w-full sm:w-2/3'>
+                <p className='mt-4'>
                     Do zprávy můžete uvést podrobnosti o vašem projektu. Pokud máte k dispozici fotografie nebo technickou dokumentaci (ve formátech JPG, JPEG, PNG, GIF nebo PDF), můžete je přiložit pomocí funkce nahrávání souborů.
                 </p>
-                <p className='w-full sm:w-2/3'>
+                <p className='mt-4'>
                     Pokud již máte svůj 3D model (STL), můžete jej nahrát prostřednictvím našeho{' '}
                     <Link href={'/poptat-tisk'}>
                         Kalkulátoru
